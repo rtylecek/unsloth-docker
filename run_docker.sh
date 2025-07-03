0f84abc30f7b7ff182b46f796073dad7817043c0
Radim Tylecek <radim.tylecek@certicon.cz>
2025-07-03 14:57:29 +0200

    Update usage instructions and fix volume mapping in run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -e
 if [ "$1" == "-h" ]; then
-  echo "Usage: `basename $0` [version] [gpu id] [output path]"
+  echo "Usage: `basename $0` [version] [gpu id] [data path] [train path]"
   exit 0
 fi
 
 echo "unsloth-docker: starting v$1 on GPU#$2"
-export HOST_DATA=$3
 export USER_HOME="/home/unsloth"
 export HF_DATA="$USER_HOME/.cache/huggingface/"
 export TRAIN_PATH="$USER_HOME/train"
@@ -13,10 +12,10 @@ export TRAIN_PATH="$USER_HOME/train"
 # -v $PWD:$USER_HOME
 docker run -it --rm \
     --user $(id -u):$(id -g) \
-    --volume $HOST_DATA:$HF_DATA --volume $PWD:$TRAIN_PATH \
+    --volume $3:$HF_DATA --volume $4:$TRAIN_PATH \
     --gpus device=$2 \
     --network=host \
     unsloth:$1 \
     conda run -n unsloth_env \
     python3 unsloth_trainer.py \
-    $3 $4 $5 $6 $7 $8 $9
+    $5 $6 $7 $8 $9
